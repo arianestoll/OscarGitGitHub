d54eeb1d14d0910f45e58cf6370e22c5c9b94552
Hakan Gok <hakangok1974@yahoo.com>
2022-04-03 10:56:19 +0100

    second commit

--- src/day02/TestCase02.java
+++ src/day02/TestCase02.java
@@ -4,6 +4,6 @@ public class TestCase02 {
 
     public static void main(String[] args) {
         System.out.println("Amazon log in test");
-
+        System.out.println("I 'm adding a change on this one");
     }
 }
