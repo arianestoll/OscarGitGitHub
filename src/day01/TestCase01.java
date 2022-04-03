57bba8cdcf9a156b9309fef9641ea16821fc349d
Hakan Gok <hakangok1974@yahoo.com>
2022-04-03 10:55:27 +0100

    second commit

--- src/day01/TestCase01.java
+++ src/day01/TestCase01.java
@@ -3,5 +3,6 @@ package day01;
 public class TestCase01 {
     public static void main(String[] args) {
         System.out.println(" let's add a change");
+        System.out.println("I'm adding another change");
     }
 }
