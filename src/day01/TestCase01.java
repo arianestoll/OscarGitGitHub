5febab024fc4736186cf352b1665fa23e35ec7e0
Hakan Gok <hakangok1974@yahoo.com>
2022-04-03 11:53:52 +0100

    add after linking to github

--- src/day01/TestCase01.java
+++ src/day01/TestCase01.java
@@ -4,5 +4,6 @@ public class TestCase01 {
     public static void main(String[] args) {
         System.out.println(" let's add a change");
         System.out.println("I'm adding another change");
+        System.out.println("making a change on oscar's project-case 1");
     }
 }
